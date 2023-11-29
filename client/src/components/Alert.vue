eff9a29e8fbe21cd74fb0538a79f02b9b81569f1
Knight-wh <yffcbgh@163.com>
2023-11-29 15:06:46 +0800

    update Alert compnent

--- client/src/components/Alert.vue
+++ client/src/components/Alert.vue
@@ -1,12 +1,20 @@
 <template>
   <div>
-    <div class="alert alert-success" role="alert">{{ message }}</div>
+    <div class="alert alert-dismissible fade show" :class="alertStyle" role="alert">
+      {{ message }}
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
     <br/>
   </div>
 </template>
 
 <script>
 export default {
-  props: ['message'],
+  props: ['message','type'],
+  computed: {
+    alertStyle() {
+      return `alert-${this.type}`;
+    }
+  }
 };
 </script>
